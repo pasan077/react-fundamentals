02927e94c604faa252b86cb6e37e1458f4cabe64
Pasan Jayathilaka <pasan4all@gmail.com>
2020-12-14 10:47:01 +0530

    14-12-2020 1046hrs
    14-12-2020 1046hrs

--- src/components/NameList/NameList.jsx
+++ src/components/NameList/NameList.jsx
@@ -7,11 +7,11 @@ function NameList(){
             <h1>Name List</h1>
       <hr/>
             <ul>
-                <NameListItem name="Pasan Jayathilaka" course="Sycoshouts.com" />
-                <NameListItem name="Uthpala Heenatigala" course="DoingITeasy.com" />
-                <NameListItem name="Lahiru Perera" course="RocellTiles" />
-                <NameListItem name="Prashan Palansooriya" course="onetyremart.com" />
-                <NameListItem />
+                <NameListItem name="Pasan Jayathilaka" course="Sycoshouts.com" email="pasan4all@gmail.com"/>
+                <NameListItem name="Uthpala Heenatigala" course="DoingITeasy.com" email="uheenatigala@gmail.com"/>
+                <NameListItem name="Lahiru Perera" course="RocellTiles" email="Lahiru.p@gmail.com"/>
+                <NameListItem name="Prashan Palansooriya" course="onetyremart.com" email="prashanp@gmail.com"/>
+                <NameListItem name="Kasun Lakmal" course="business entreprenuer" email="Kasun.lakmal@gmail.com"/>
             </ul>
         </div>
     )
--- src/components/NameList/NameListItem.jsx
+++ src/components/NameList/NameListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NameListItem (props) {
     return (
         <div>
-            <li>{props.name} - course - {props.course}</li>
+            <li>{props.name} - course - {props.course} - email - {props.email}</li>
         </div>
     )
 }
