8d527b905e181a2efa13b72772e3da17629bdbb3
Pasan Jayathilaka <pasan4all@gmail.com>
2020-12-14 13:41:35 +0530

    14-12-2020 1341hrs
    14-12-2020 1341hrs

--- src/components/NameList/NameList.jsx
+++ src/components/NameList/NameList.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import NameListItem from './NameListItem';
 
 function NameList(){
+
+    const nameList = [{
+        "name": {
+        "title": "mr",
+        "first": "brad",
+        "last": "gibson"
+      },
+      "location": {
+        "street": "9278 new road",
+        "city": "kilcoole",
+        "state": "waterford",
+        "postcode": "93027",
+     },
+     "email": "brad.gibson@example.com",
+     "dob": {
+        "date": "1993-07-20T09:44:18.674Z",
+        "age": 26
+      },
+      "picture": {
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+      },
+    },
+];
+
     return (
         <div>
             <h1>Name List</h1>
       <hr/>
             <ul>
-                <NameListItem name="Pasan Jayathilaka" course="Sycoshouts.com" email="pasan4all@gmail.com"/>
-                <NameListItem name="Uthpala Heenatigala" course="DoingITeasy.com" email="uheenatigala@gmail.com"/>
-                <NameListItem name="Lahiru Perera" course="RocellTiles" email="Lahiru.p@gmail.com"/>
-                <NameListItem name="Prashan Palansooriya" course="onetyremart.com" email="prashanp@gmail.com"/>
-                <NameListItem name="Kasun Lakmal" course="business entreprenuer" email="Kasun.lakmal@gmail.com"/>
-            </ul>
-        </div>
+                <NameListItem 
+                name={nameList[0].name.first + ' ' + nameList[0].name.last} 
+                name={`${nameList[0].name.first} ${nameList[0].name.last}`} 
+                city={nameList[0].location.city} 
+                email={nameList[0].email}
+                birthday={nameList[0].dob.date}
+                avatar={nameList[0].picture.medium}/>
+                 </ul>
+        </div> 
     )
 }
 
--- src/components/NameList/NameListItem.jsx
+++ src/components/NameList/NameListItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import moment from 'moment';
 
 function NameListItem (props) {
     return (
         <div>
-            <li>{props.name} - course - {props.course} - email - {props.email}</li>
+            <li>
+                <p><img src={props.avatar}/> {props.name}</p>
+                <p>City: {props.city}</p>
+                <p>Email: {props.email}</p>
+                <p>Birthday:{ } {new Intl.DateTimeFormat('si-lk').format(new Date(props.birthday))}</p>
+                <p></p>
+                <p>Birthday: {moment(props.birthday).format('DD-MM-YYYY')}</p>
+            </li>
         </div>
     )
 }
